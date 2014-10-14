7ffa3ec336169febbc34da6cc9f77fb5494f3a10
sveluri <sveluri@deloitte.com>
2014-10-14 12:20:50 +0530

    Add name space to super api model classes

---
+++ shared/src/main/java/org/familysearch/gal/shared/model/package-info.java
@@ -0,0 +1,7 @@
+@javax.xml.bind.annotation.XmlSchema(
+    namespace = "http://api.gal.familysearch.org/model",
+    xmlns = {@javax.xml.bind.annotation.XmlNs(prefix = "fs-gal", namespaceURI = "http://api.gal.familysearch.org/model")},
+    elementFormDefault = javax.xml.bind.annotation.XmlNsForm.UNQUALIFIED
+)
+package org.familysearch.gal.shared.model;
+
