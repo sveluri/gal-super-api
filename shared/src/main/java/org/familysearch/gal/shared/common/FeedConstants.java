b1dfef990d925a317b27f6aa458dd62ff9739393
sveluri <sveluri@deloitte.com>
2014-10-14 15:10:39 +0530

    Add FeedConstants and move commonly used strings to it.

---
+++ shared/src/main/java/org/familysearch/gal/shared/common/FeedConstants.java
@@ -0,0 +1,12 @@
+package org.familysearch.gal.shared.common;
+
+/**
+ * Constants used in creation of Feed
+ */
+public class FeedConstants {
+
+    public static final String PAGE = "page";
+    public static final String PAGE_SIZE = "pagesize";
+    public static final String TYPE = "type";
+
+}
